c5b5939cbf3788d06f69b8cdf3590f92d1431ee8
Isaiah Larsen <isaiahlarsen01@gmail.com>
2018-11-25 22:03:13 -0700

    Shortest job first added

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,7 @@ public class Main {
         int[] start = new int[]{0, 0, 0};
         int[] run = new int[]{24,3,3};
         fcfs(start,run);
+        sjf(start, run);
 //        ArrayList<Integer> start = new ArrayList<>();
 //
 //        while((data = in.readLine()) != null){
@@ -29,6 +30,63 @@ public class Main {
 
     }
 
+    /*
+    * Shortest Job first
+    * @params array of ints for start times and array of ints for run times
+    * */
+    private static void sjf(int[] s, int[] r){
+        int clock = 0;
+        int ta = 0;
+        int wait = 0;
+        int resp = 0;
+        int ta_total = 0;
+        int wait_total = 0;
+        int resp_total = 0;
+        int numProcess = s.length;
+        int[] sortedStart = new int[s.length];
+        int[] sortedRun = new int[r.length];
+        for(int i = 0; i < s.length; i++){
+            sortedStart[i] = s[i];
+        }
+        for(int i = 0; i < r.length; i++){
+            sortedRun[i] = r[i];
+        }
+        order(sortedStart,sortedRun);
+        for(int i = 0; i < sortedStart.length; i++){
+            ta = (clock + sortedRun[i]) - sortedStart[i];
+            wait = ta - sortedRun[i];
+            resp = clock - sortedStart[i];
+            clock += sortedRun[i];
+            ta_total += ta;
+            wait_total += wait;
+            resp_total += resp;
+
+        }
+        System.out.println("Shortest Job First");
+        printAverages(ta_total, wait_total, resp_total, numProcess);
+
+    }
+    private static void order(int[] s, int[] r){
+        int temp;
+        int temp2;
+        for (int i = 1; i < r.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (r[j] < r [j - 1]) {
+                    temp = r[j];
+                    temp2 = s[j];
+                    r[j] = r[j - 1];
+                    s[j] = s[j - 1];
+                    r[j - 1] = temp;
+                    s[j - 1] = temp2;
+                }
+            }
+        }
+    }
+
+    /*
+    * First come first serve algorithm
+    * @params array of ints for start times and array of ints for run times
+    * */
     private static void fcfs(int[] s, int[] r) {
         int clock = 0;
         int ta = 0;
@@ -49,8 +107,15 @@ public class Main {
             resp_total += resp;
 
         }
-        System.out.println("Average TA: " + ta_total/numProcess + " Average Wait: " + wait_total/numProcess + " Average Response: " + resp_total/numProcess);
+        System.out.println("First Come First Serve:");
+        printAverages(ta_total, wait_total, resp_total, numProcess);
     }
 
-
+    /*
+    * Method to print out average results of Turn around, wait, and response time
+    * @params total turn around time, wait, and response times, and total number of processes
+    * */
+    private static void printAverages(int t, int w, int r, int n){
+        System.out.println("Average TA: " + t/n + " Average Wait: " + w/n + " Average Response: " + r/n);
+    }
 }

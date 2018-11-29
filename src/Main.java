105274e4522c77c9110ea9670117055a72219873
Isaiah Larsen <isaiahlarsen01@gmail.com>
2018-11-28 21:17:19 -0700

    Shortest remaining time added

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String data;
-        int[] start = new int[]{0, 0, 0};
-        int[] run = new int[]{24,3,3};
+        int[] start = new int[]{0, 1, 2, 3, 4};
+        int[] run = new int[]{2, 3, 1, 5, 4};
         fcfs(start,run);
         sjf(start, run);
-//        ArrayList<Integer> start = new ArrayList<>();
+        strf(start, run);
 //
 //        while((data = in.readLine()) != null){
 //            data.split(" ");
@@ -28,6 +30,108 @@ public class Main {
 //        }
 //        System.out.println(start);
 
+    }
+
+    /**
+     * Round Robin
+     * @param s start times array
+     * @param r run times array
+     */
+    private static void roundRobin(int[] s, int[] r){
+
+    }
+
+    /**
+     *Shortest run time first
+     * @param s start times array
+     * @param r run time array
+     */
+    private static void strf(int[] s, int[] r){
+        int clock = 0;
+        int ta_total = 0;
+        int wait_total = 0;
+        int resp_total = 0;
+        int currentLowRunTime = r[0];
+        int totalRunTime = 0;
+        int numProcess = s.length;
+
+        int[] strfStart = new int[s.length];
+        int[] strfRun = new int[r.length];
+        for(int i = 0; i < s.length; i++){
+            strfStart[i] = s[i];
+        }
+        for(int i = 0; i < r.length; i++){
+            strfRun[i] = r[i];
+        }
+
+        //add run times to get a total max on runtime
+        for(int i = 0; i < r.length; i++){
+            totalRunTime += r[i];
+        }
+
+        //index for keeping track of lowest remaining time
+        int index = 0;
+        int runNext = 0;
+        int[] firstRun = new int[strfRun.length];
+        currentLowRunTime = strfRun[index];
+        firstRun[0] = strfStart[0];
+        clock = strfStart[index + 1];
+        strfRun[index] -= strfStart[index + 1];
+        Map<Integer,Integer> queue = new HashMap<>();
+        while(clock < totalRunTime){
+            //run till next arrival
+            if(index < strfStart.length - 1) {
+                if (!queue.containsKey(index)) {
+                    queue.put(index, strfRun[index]);
+                }
+                if (!queue.containsKey(index + 1)) {
+                    queue.put(index + 1, strfRun[index + 1]);
+                }
+            }
+
+            // check which is lower from queue
+            for(Map.Entry<Integer, Integer> entry : queue.entrySet()){
+                int val = entry.getValue();
+                int key = entry.getKey();
+                if(val < currentLowRunTime && val != 0){
+                    currentLowRunTime = strfRun[key];
+                    runNext = key;
+                }
+
+
+            }
+            if(index < strfStart.length - 1) {
+                index++;
+                if(index >= strfStart.length - 1){
+                    clock += 1;
+                }else {
+                    clock += (strfStart[index + 1] - strfStart[index]);
+                }
+            }else{
+                clock += 1;
+            }
+            if((runNext) >= strfStart.length - 1){
+                strfRun[runNext] -= 1;
+            }else{
+                if(r[runNext] == strfRun[runNext]) {
+                    firstRun[runNext] = clock - 1;
+                }
+                strfRun[runNext] -= (strfStart[runNext + 1] - strfStart[runNext]);
+            }
+            //update map
+            queue.replace(runNext, strfRun[runNext]);
+            //reset lowest run time
+            if(strfRun[runNext] == 0){
+                currentLowRunTime = totalRunTime;
+                ta_total += (clock - strfStart[runNext]);
+                wait_total += ((clock - strfStart[runNext])) - r[runNext];
+                resp_total += firstRun[runNext] - strfStart[runNext];
+            }
+
+
+        }
+        System.out.println("Shortest remaining Time:");
+        printAverages(ta_total, wait_total, resp_total, numProcess);
     }
 
     /*
@@ -52,6 +156,7 @@ public class Main {
             sortedRun[i] = r[i];
         }
         order(sortedStart,sortedRun);
+        clock = sortedStart[0];
         for(int i = 0; i < sortedStart.length; i++){
             ta = (clock + sortedRun[i]) - sortedStart[i];
             wait = ta - sortedRun[i];
@@ -66,6 +171,12 @@ public class Main {
         printAverages(ta_total, wait_total, resp_total, numProcess);
 
     }
+
+    /*
+     * Sorts the run times to run shortest first but keeping the start and run times together
+     * basically anything done to runtime array is done to start array
+     * @params start time array and runtime array
+     */
     private static void order(int[] s, int[] r){
         int temp;
         int temp2;
